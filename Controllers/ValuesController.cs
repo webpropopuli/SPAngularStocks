c93f0269cf9dd39003ed9e52da69ca0f6911b716
David Marlowe <webpropopuli@gmail.com>
2018-07-06 09:43:09 -0400

    DB created and first 2 GET routes ok

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,23 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StockApp1.API.MyData;
 
 namespace StockApp1.API.Controllers
 {
     [Route("api/[controller]")]
-    [ApiController]
+ 
     public class ValuesController : ControllerBase
     {
+        private readonly StockDataContext _context;
+        public ValuesController(StockApp1.API.MyData.StockDataContext context) => _context = context;
+
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IActionResult GetValues()
         {
-            return new string[] { "value1", "value2" };
+            var values = _context.Values.ToList();
+
+            return Ok(values);  //
         }
 
         // GET api/values/5
+        // if id not found, returns a '204 no content' which is fine for now
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id) => id.ToString();
+        public IActionResult GetValue(int id)
+        {
+            var value = _context.Values.FirstOrDefault(x => x.Id == id);
+
+            return Ok(value);
+        }
 
         // POST api/values
         [HttpPost]
